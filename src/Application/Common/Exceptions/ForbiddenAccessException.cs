4220900dcc7ba20cb4b8b796b35d49ab1e81f1b4
Khac Thanh <thanhcqb2048@gmail.com>
2025-05-14 20:56:28 +0700

    Init

---
+++ src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArchitectureBase.Application.Common.Exceptions;
+
+public class ForbiddenAccessException : Exception
+{
+    public ForbiddenAccessException() : base() { }
+}
---
+++ src/Web/Endpoints/Users.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitectureBase.Infrastructure.Identity;
+
+namespace CleanArchitectureBase.Web.Endpoints;
+
+public class Users : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapIdentityApi<ApplicationUser>();
+    }
+}
